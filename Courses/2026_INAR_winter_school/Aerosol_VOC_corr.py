9747e19be4c100a0b9c95104fbe7c9c280073685
Normann25 <151392271+Normann25@users.noreply.github.com>
2026-03-11 08:39:50 +0100

    Update Aerosol_VOC_corr.py

--- Courses/2026_INAR_winter_school/Aerosol_VOC_corr.py
+++ Courses/2026_INAR_winter_school/Aerosol_VOC_corr.py
@@ -19,13 +19,31 @@ HYDE_data = pd.merge(HYDE_data, data['HYDE_OVOC_clean'], on = 'Time', how = 'out
 #%%
 # NAIS OVOC correlations
 OVOC_keys = ['Monomers', 'NitrogenMonomers', 'Dimers', 'NitrogenDimers', 'TotalOrganics', 'DMSppb', 'IPppb', 'MTppb']
+cluster_keys = ['HSO4-', '(H2SO4)HSO4-', '(H2SO4)2HSO4-', 'IO3-', '(HIO3)HSO4-']
+PF_vs_cluster_keys = []
+for key in cluster_keys:
+    TZS_data[f'J2-2.3-_vs_{key}'] = TZS_data['J2-2.3-'] / TZS_data[key]
+    PF_vs_cluster_keys.append(f'J2-2.3-_vs_{key}')
 Marine_mask = TZS_data['Marine'] == 1
 Land_mask = TZS_data['Land'] == 1
 
 comparison_dict = {'HYDE': HYDE_data, 'TZS Marine': TZS_data[Marine_mask], 'TZS Land': TZS_data[Land_mask]}
 colors = ['orange', 'mediumblue', 'green']
-ax_labels = ['J$_{2-2.3 nm}$/N$_{<2 nm}$ (s$^{-1}$)', 'Monomers (molecules cm$^{-3}$)', 'N monomers (molecules cm$^{-3}$)', 
-             'Dimers (molecules cm$^{-3}$)', 'N dimers (molecules cm$^{-3}$)', 'Total org. (molecules cm$^{-3}$)', 
+#%%
+ax_labels = ['J$_{2-2.3 nm}$/N$_{<2 nm}$ (# s$^{-1}$)', 'Monomers (# cm$^{-3}$)', 'N monomers (# cm$^{-3}$)', 
+             'Dimers (# cm$^{-3}$)', 'N dimers (# cm$^{-3}$)', 'Total org. (# cm$^{-3}$)', 
              'DMS (ppb)', 'Isoprene (ppb)', 'Monoterpenes (ppb)']
-comparison_dict = plot_seasonal_scatter(comparison_dict, ['HYDE', 'TZS Marine', 'TZS Land'], ['J2-2.3-/N<2-']+OVOC_keys, 
-                      ['2024-01-01 00:00', '2025-12-31 23:59'], colors, ax_labels, 'Day', 'Figures/Correlations/')
+seasonal_corr = plot_seasonal_scatter(comparison_dict, ['HYDE', 'TZS Marine', 'TZS Land'], ['J2-2.3-/N<2-']+OVOC_keys, 
+                      ['2024-01-01 00:00', '2025-12-31 23:59'], colors, ax_labels, 'Day', 0.05, 0.05, 'Figures/Correlations/')
+
+for label, key in zip(ax_labels[1:6], OVOC_keys[:5]):
+    Monoterpenes_vs_OVOC = plot_seasonal_scatter(comparison_dict, ['HYDE', 'TZS Marine', 'TZS Land'], [OVOC_keys[5]]+[key], 
+                                ['2024-01-01 00:00', '2025-12-31 23:59'], colors, [ax_labels[6], label], 'Day', 0.62, 0.05, 'Figures/Correlations/')
+    Monoterpenes_vs_OVOC = plot_seasonal_scatter(comparison_dict, ['HYDE', 'TZS Marine', 'TZS Land'], [OVOC_keys[7]]+[key], 
+                                ['2024-01-01 00:00', '2025-12-31 23:59'], colors, [ax_labels[8], label], 'Day', 0.62, 0.05, 'Figures/Correlations/')
+#%%
+print(comparison_dict['TZS Land'].keys())
+ax_labels = ['Temperature (C)', 'J$_{2-2.3 nm}$/HSO$_{4}^{-}$', 'J$_{2-2.3 nm}$/(H$_{2}$SO$_{4}$)HSO$_{4}^{-}$', 'J$_{2-2.3 nm}$/(H$_{2}$SO$_{4}$)$_{2}$HSO$_{4}^{-}$',
+             'J$_{2-2.3 nm}$/IO$_{3}^{-}$', 'J$_{2-2.3 nm}$/(HIO$_{3}$)HSO$_{4}^{-}$']
+T_vs_PFR = plot_seasonal_scatter(comparison_dict, ['TZS Marine', 'TZS Land'], ['avg_T_C']+PF_vs_cluster_keys, 
+                                 ['2024-01-01 00:00', '2025-12-31 23:59'], colors[1:], ax_labels, 'Day', 0.05, 0.05, 'Figures/Correlations/')
