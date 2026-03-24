e8835a518fc75075824d848d48956ae5ab54f148
Normann25 <151392271+Normann25@users.noreply.github.com>
2026-03-24 11:01:45 +0100

    Update TUV.py

--- TUV spectra/TUV.py
+++ TUV spectra/TUV.py
@@ -15,6 +15,10 @@ data = import_data(path, '', None, None, None)
 
 equator_keys = [['250621_equator_0km', '250621_equator_05km', '250621_equator_1km', '250621_equator_5km', '250621_equator_10km'], 
                 ['251221_equator_0km', '251221_equator_05km', '251221_equator_1km', '251221_equator_5km', '251221_equator_10km']]
+arktis_keys = [['250621_arktis_0km', '250621_arktis_05km', '250621_arktis_1km', '250621_arktis_5km', '250621_arktis_10km'], 
+               ['251221_arktis_0km', '251221_arktis_05km', '251221_arktis_1km', '251221_arktis_5km', '251221_arktis_10km']]
+marselis_keys = [['250621_marselis_0km', '250621_marselis_05km', '250621_marselis_1km', '250621_marselis_5km', '250621_marselis_10km'], 
+                 ['251221_marselis_0km', '251221_marselis_05km', '251221_marselis_1km', '251221_marselis_5km', '251221_marselis_10km']]
 dates = ['Jun 21st 2025', 'Dec 21st 2025']
 altitudes = ['0 km', '0.5 km', '1 km', '5 km', '10 km']
 #%%
@@ -30,5 +34,38 @@ for i, keys in enumerate(equator_keys):
     ax[i].legend()
     ax[i].set(title = dates[i], xlabel = 'Wavelength (nm)', ylabel = 'Actinic flux (photon s$^{-1}$cm$^{-2}$nm$^{-1}$)')
 
+fig.suptitle('Equator')
 fig.tight_layout()
 fig.savefig('Solar_spectrum_equator.jpg', dpi = 600)
+#%%
+fig, ax = plt.subplots(2, 1, figsize = (6.3, 6))
+for i, keys in enumerate(arktis_keys):
+    n_lines = len(keys)+1
+    cmap = mpl.colormaps['viridis']
+    colors = cmap(np.linspace(0, 1, n_lines))
+    for j, key in enumerate(keys):
+        wavelength = (data[key]['LOWER WVL'] + data[key]['UPPER WVL']) / 2
+        ax[i].plot(wavelength, data[key]['TOTAL'], label = altitudes[j], color = colors[j], lw = 1)
+
+    ax[i].legend()
+    ax[i].set(title = dates[i], xlabel = 'Wavelength (nm)', ylabel = 'Actinic flux (photon s$^{-1}$cm$^{-2}$nm$^{-1}$)')
+
+fig.suptitle('Arctic')
+fig.tight_layout()
+fig.savefig('Solar_spectrum_arctic.jpg', dpi = 600)
+#%%
+fig, ax = plt.subplots(2, 1, figsize = (6.3, 6))
+for i, keys in enumerate(marselis_keys):
+    n_lines = len(keys)+1
+    cmap = mpl.colormaps['viridis']
+    colors = cmap(np.linspace(0, 1, n_lines))
+    for j, key in enumerate(keys):
+        wavelength = (data[key]['LOWER WVL'] + data[key]['UPPER WVL']) / 2
+        ax[i].plot(wavelength, data[key]['TOTAL'], label = altitudes[j], color = colors[j], lw = 1)
+
+    ax[i].legend()
+    ax[i].set(title = dates[i], xlabel = 'Wavelength (nm)', ylabel = 'Actinic flux (photon s$^{-1}$cm$^{-2}$nm$^{-1}$)')
+
+fig.suptitle('Marselis Forrest')
+fig.tight_layout()
+fig.savefig('Solar_spectrum_marselis.jpg', dpi = 600)
