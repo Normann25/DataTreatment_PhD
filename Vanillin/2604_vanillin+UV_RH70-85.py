84b4820d1f6edb509e0cfbb955e5557f8d07c4b3
Normann25 <151392271+Normann25@users.noreply.github.com>
2026-06-18 16:17:31 +0200

    Update 2604_vanillin+UV_RH70-85.py

--- Vanillin/2604_vanillin+UV_RH70-85.py
+++ Vanillin/2604_vanillin+UV_RH70-85.py
@@ -19,23 +19,38 @@ timestamps = [['2026-04-27 10:36', '2026-04-27 17:28'],
               ['2026-04-29 08:52', '2026-04-29 16:45'],
               ['2026-04-30 07:58', '2026-04-30 17:56']]
 t_zero = ['2026-04-27 12:16', '2026-04-28 12:35', '2026-04-29 11:45', '2026-04-30 12:47']
-t_UV_off = ['2026-04-28 16:36', '2026-04-29 15:45', '2026-04-30 16:47']
+t_UV_off = ['2026-04-27 16:21', '2026-04-28 16:36', '2026-04-29 15:45', '2026-04-30 16:47']
+HEPA_timestamps = [['2026-04-27 08:40', '2026-04-27 09:00'],
+                   ['2026-04-28 07:55', '2026-04-28 08:15'],
+                   ['2026-04-29 08:40', '2026-04-29 09:00'],
+                   ['2026-04-30 08:15', '2026-04-30 08:35']]
+
 RH = ['70% RH', '70% RH', '85% RH', '85% RH']
 
 SMPS = {}
 PTRMS = {}
+AMS = {}
 DAQ = {}
 for t, path in zip(t_zero, paths):
     temp_SMPS = import_SMPS(f'{parent_path}{path}SMPS/', '', 0)
     for key in temp_SMPS.keys():
-        temp_SMPS[key].loc[temp_SMPS[key]['Time'] < pd.to_datetime(t), ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = np.nan
-        SMPS[key] = temp_SMPS[key]
+        temp_SMPS[key].loc[temp_SMPS[key]['Time'] < pd.to_datetime(t) + pd.Timedelta(minutes = 20), ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = 0
+        temp = remove_spikes_up(temp_SMPS[key], ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)'], 20)
+        SMPS[key] = temp
+        # SMPS[key] = temp_SMPS[key]
     if path != paths[0]:
         temp_PTR = import_PTRMS(f'{parent_path}{path}PTRMS/', '')
         for key in temp_PTR.keys():
             mask = (0 < temp_PTR[key][temp_PTR[key].keys()[5]]) & (temp_PTR[key][temp_PTR[key].keys()[5]] < 90)
             temp = temp_PTR[key][mask]
             PTRMS[key] = temp
+    temp_AMS = import_data(f'{parent_path}{path}AMS/', '', 't_series', '%d-%m-%Y %H:%M:%S', 0)
+    for key in temp_AMS.keys():
+        if 'PToF' not in key:
+            temp_AMS[key].columns = ['t_series', 'HROrg', 'HRNO3', 'HRSO4', 'HRNH4', 'HRChl', 'Ratio_H_C', 'Ratio_O_C', 
+                            'familyCHN', 'familyCHO1', 'familyCHO1N', 'familyCH', 'f43', 'f44', 'Time']
+            # temp_AMS[key] = remove_spikes(temp_AMS[key], ['HROrg', 'HRNO3', 'HRSO4', 'HRNH4', 'HRChl', 'familyCHN', 'familyCHO1', 'familyCHO1N', 'familyCH'], 0.6)
+        AMS[key] = temp_AMS[key]
     temp_daq = import_data(f'{parent_path}{path}DAQ/', '', 'DAQ_Timestamp_UTC', '%d-%m-%Y %H:%M:%S', 0)
     for key in temp_daq.keys():
         DAQ[key] = temp_daq[key]
@@ -45,17 +60,25 @@ save_path = 'Figures/2604_vanillin+UV_RH70-85/'
 for key in SMPS.keys():
     SMPS[key].rename(columns = {SMPS[key].columns[38]:'Total concentration'}, inplace = True)
     SMPS[key] = SMPS[key].fillna(0)
-#%%
+
 SMPS_keys = [['260427_vanillin+UV_RH70_number', '260428_vanillin+UV_RH70_number', '260429_vanillin+UV_RH85_number', '260430_vanillin+UV_RH85_number'],
              ['260427_vanillin+UV_RH70_mass', '260428_vanillin+UV_RH70_mass', '260429_vanillin+UV_RH85_mass', '260430_vanillin+UV_RH85_mass']]
-
+AMS_keys = ['260427_AMS_vanillin+UV_70RH_TS', '260428_AMS_vanillin+UV_70RH_TS', '260429_AMS_vanillin+UV_85RH_TS', '260430_AMS_vanillin+UV_85RH_TS']
+PTRMS_keys = ['260428_VL+UV_70RH_initial', '260429_VL+UV_RH85_initial', '260430_VL+UV_RH85_inital']
+DAQ_keys = ['DataDAQ_260427', 'DataDAQ_260428', 'DataDAQ_260429', 'DataDAQ_260430']
+#%%
 ax, ax_2 = plot_SMPS(SMPS, SMPS_keys, SMPS['260428_vanillin+UV_RH70_mass'].columns[42:-1], 'number and mass', 
                      timestamps, 10, RH, 'Total concentration', t_zero, 2, 2, save_path)
 #%%
-PTRMS_keys = ['260428_VL+UV_70RH_initial', '260429_VL+UV_RH85_initial', '260430_VL+UV_RH85_inital']
+for i, key in enumerate(AMS_keys):
+    plot_AMS(AMS[key], None, t_zero[i], timestamps[i], HEPA_timestamps[i], 1, RH[i], save_path)
+#%%
 for i, key in enumerate(PTRMS_keys):
     fig, ax = plot_PTRMS_decay(PTRMS[key], PTRMS[key].keys()[5], [PTRMS[key].keys()[4]], 
                                ['C$_{8}$H$_{9}$O$_{3}^{+}$', 'C$_{7}$H$_{9}$O$_{2}^{+}$'], 
                                t_zero[i+1], t_UV_off[i], timestamps[i+1][1], RH[i+1])
     fig.tight_layout()
     fig.savefig(f'{save_path}{t_zero[i+1].split(' ')[0]}_PTRMS_initial.jpg', dpi = 600)
+#%%
+for i, time in enumerate(timestamps):
+    plot_AURA_overview(DAQ[DAQ_keys[i]], SMPS[SMPS_keys[0][i]], AMS[AMS_keys[i]], time, t_zero[i], save_path)
