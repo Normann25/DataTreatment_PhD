11f2f9d9748733d484687ba316b3258556f0a529
Normann25 <151392271+Normann25@users.noreply.github.com>
2026-06-18 15:39:25 +0200

    Update 2604_vanillin+UV_dry.py

--- Vanillin/2604_vanillin+UV_dry.py
+++ Vanillin/2604_vanillin+UV_dry.py
@@ -5,7 +5,6 @@ from read_data_functions import *
 from plot_functions import *
 from calculations import *
 from grouping import *
-from scipy.signal import find_peaks
 plt.style.use('../Style.mplstyle')
 import warnings
 warnings.filterwarnings('ignore')
@@ -25,44 +24,6 @@ HEPA_timestamps = [['2026-04-21 08:40', '2026-04-21 09:00'],
                    ['2026-05-01 08:50', '2026-05-01 09:10'],
                    ['2026-05-04 08:10', '2026-05-04 08:30']]
 
-def remove_spikes(df, df_keys, value):
-    """
-    Parameters
-    ----------
-    df: pandas dataframe
-
-    df_keys: dataframe keys
-
-    value: float or int
-        Peak detection threshold
-    """
-
-    for key in df_keys:
-        # `prominence` sets minimum height above surrounding 
-        # signal at which a given value is considered a peak
-        peak_idx = find_peaks(df[key], prominence=value)[0]
-
-        # To detect valleys deeper than `value`, 
-        # run find_peaks on negative of data
-        valley_idx = find_peaks(-df['Admissions to Date'], prominence=value)[0]
-
-        # Combine indexes of peaks and valleys into a single array
-        idx = np.concatenate((peak_idx, valley_idx))
-
-        # Build an indicator column of peaks and valleys, or outliers
-        df['outlier'] = False
-        df.loc[idx, 'outlier'] = True
-
-        # Replace each outlier value with NaN
-        df.loc[df['outlier'], key] = np.nan
-
-        # Interpolate over NaNs just created with default linear method
-        df[key] = (df[key].interpolate().astype(float))
-
-        df = df.drop(['outlier'], axis = 1)
-
-    return df
-
 SMPS = {}
 PTRMS = {}
 AMS = {}
@@ -70,28 +31,32 @@ DAQ = {}
 for t, path in zip(t_zero, paths):
     temp_smps = import_SMPS(f'{parent_path}{path}SMPS/', '', 0)
     for key in temp_smps.keys():
-        temp = remove_spikes(temp_smps[key], [temp_smps[key].keys()[38]], max(temp_smps[key][temp_smps[key].keys()[38]])/10)
-        temp.loc[temp['Time'] < pd.to_datetime(t), ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = 0
+        if 'mass' in key:
+            temp = remove_spikes(temp_smps[key], [temp_smps[key].keys()[38]], max(temp_smps[key][temp_smps[key].keys()[38]])/4)
+        else:
+            temp = temp_smps[key]
+        temp.loc[temp['Time'] < pd.to_datetime(t) + pd.Timedelta(minutes = 30), ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = 0
         temp.loc[temp[temp_smps[key].keys()[38]] == 0, ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = 0
         temp.loc[temp['Geo. Mean (nm)'] > 100, ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = 0
-        temp = remove_spikes(temp, ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)'], 10)
+        temp = remove_spikes(temp, ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)'], 12)
         SMPS[key] = temp
     temp_PTR = import_PTRMS(f'{parent_path}{path}PTRMS/', '')
     for key in temp_PTR.keys():
-        mask = (0 < temp_PTR[key][temp_PTR[key].keys()[5]]) & (temp_PTR[key][temp_PTR[key].keys()[5]] < 90)
-        temp = temp_PTR[key][mask]
+        # mask = (0 < temp_PTR[key][temp_PTR[key].keys()[5]]) & (temp_PTR[key][temp_PTR[key].keys()[5]] < 90)
+        # temp = temp_PTR[key][mask]
+        temp = remove_spikes(temp_PTR[key], temp_PTR[key].keys()[4:-2], 5)
         PTRMS[key] = temp
     temp_AMS = import_data(f'{parent_path}{path}AMS/', '', 't_series', '%d-%m-%Y %H:%M:%S', 0)
     for key in temp_AMS.keys():
         if 'PToF' not in key:
             temp_AMS[key].columns = ['t_series', 'HROrg', 'HRNO3', 'HRSO4', 'HRNH4', 'HRChl', 'Ratio_H_C', 'Ratio_O_C', 
                             'familyCHN', 'familyCHO1', 'familyCHO1N', 'familyCH', 'f43', 'f44', 'Time']
-            mask = temp_AMS[key]['HROrg'] != 0
-            temp_AMS[key] = temp_AMS[key][mask]
+            temp_AMS[key] = remove_spikes(temp_AMS[key], ['HROrg', 'HRNO3', 'HRSO4', 'HRNH4', 'HRChl', 'familyCHN', 'familyCHO1', 'familyCHO1N', 'familyCH'], 0.6)
         AMS[key] = temp_AMS[key]
     temp_daq = import_data(f'{parent_path}{path}DAQ/', '', 'DAQ_Timestamp_UTC', '%d-%m-%Y %H:%M:%S', 0)
     for key in temp_daq.keys():
-        DAQ[key] = temp_daq[key]
+        temp = remove_spikes(temp_daq[key], ['Temp_C', 'RH_Percent'], 5)
+        DAQ[key] = temp
 
 save_path = 'Figures/2604_vanillin+UV_dry/'
 
