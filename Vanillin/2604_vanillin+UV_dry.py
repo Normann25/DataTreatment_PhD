06e1f6b119b23adf4f764754e5e5b333d756448f
Normann25 <151392271+Normann25@users.noreply.github.com>
2026-06-23 21:30:10 +0200

    Update 2604_vanillin+UV_dry.py

--- Vanillin/2604_vanillin+UV_dry.py
+++ Vanillin/2604_vanillin+UV_dry.py
@@ -25,12 +25,14 @@ HEPA_timestamps = [['2026-04-21 08:40', '2026-04-21 09:00'],
                    ['2026-05-04 08:10', '2026-05-04 08:30']]
 
 SMPS = {}
+SMPS_raw = {}
 PTRMS = {}
 AMS = {}
 DAQ = {}
 for t, path in zip(t_zero, paths):
     temp_smps = import_SMPS(f'{parent_path}{path}SMPS/', '', 0)
     for key in temp_smps.keys():
+        SMPS_raw[key] = temp_smps[key]
         if 'mass' in key:
             temp = remove_spikes_up(temp_smps[key], [temp_smps[key].keys()[38]], max(temp_smps[key][temp_smps[key].keys()[38]])/4)
         else:
@@ -58,7 +60,7 @@ for t, path in zip(t_zero, paths):
         temp = remove_spikes(temp_daq[key], ['Temp_C', 'RH_Percent'], 5)
         DAQ[key] = temp
 
-save_path = 'Figures/2604_vanillin+UV_dry/'
+save_path = '../../../Figures/Vanillin/2604_vanillin+UV_dry/'
 
 for key in SMPS.keys():
     SMPS[key].rename(columns = {SMPS[key].columns[38]:'Total concentration'}, inplace = True)
@@ -70,18 +72,49 @@ AMS_keys = ['260421_AMS_vanillin+UV_dry_TS', '260422_AMS_vanillin+UV_dry_TS', '2
 PTRMS_keys = ['260501_VL+UV_dry_initial', '260504_VL+UV_dry_initial']
 DAQ_keys = ['DataDAQ_260421', 'DataDAQ_260422', 'DataDAQ_260501', 'DataDAQ_260504']
 #%%
+# Experiment overview
+for i, time in enumerate(timestamps):
+    plot_AURA_overview(DAQ[DAQ_keys[i]], SMPS[SMPS_keys[0][i]], AMS[AMS_keys[i]], time, t_zero[i], 'Dry', save_path)
+#%%
+# SMPS raw
+for i, time in enumerate(timestamps):
+    df_number = time_filtered_conc(SMPS_raw[SMPS_keys[0][i]], [SMPS_raw[SMPS_keys[0][i]].keys()[38], 'Geo. Mean (nm)'], time)
+    df_mass = time_filtered_conc(SMPS_raw[SMPS_keys[1][i]], [SMPS_raw[SMPS_keys[1][i]].keys()[38], 'Geo. Mean (nm)'], time)
+    print(df_mass)
+    fig, ax = plt.subplots(2, 1, figsize = (6.3, 6.3))
+
+    plot_total(ax[0], df_number, SMPS_raw[SMPS_keys[0][i]].keys()[38], 'purple', t_zero[i])
+    ax[0].set_ylabel('Total number conc. (# cm$^{-3}$)', color = 'purple')
+    ax[0].tick_params(axis = 'y', labelcolor = 'purple')
+    ax0_twin = ax[0].twinx()
+    plot_total(ax0_twin, df_number, 'Geo. Mean (nm)', 'green', t_zero[i])
+    ax0_twin.set_ylabel('Geo. mean D$_{p}$ (nm)', color = 'green')
+    ax0_twin.tick_params(axis = 'y', labelcolor = 'green')
+
+    plot_total(ax[1], df_mass, SMPS_raw[SMPS_keys[1][i]].keys()[38], 'purple', t_zero[i])
+    ax[1].set_ylabel('Total mass conc. ($\mu$g m$^{-3}$)', color = 'purple')
+    ax[1].tick_params(axis = 'y', labelcolor = 'purple')
+    ax1_twin = ax[1].twinx()
+    plot_total(ax1_twin, df_mass, 'Geo. Mean (nm)', 'green', t_zero[i])
+    ax1_twin.set_ylabel('Geo. mean D$_{p}$ (nm)', color = 'green')
+    ax1_twin.tick_params(axis = 'y', labelcolor = 'green')
+
+    fig.suptitle(f'{t_zero[i].split(' ')[0]}, Dry', fontsize = 14)
+    fig.tight_layout()
+    fig.savefig(f'{save_path}{t_zero[i].split(' ')[0]}_SMPS_TS_raw.jpg', dpi = 600)
+#%%
+# SMPS spikes removed
 ax, ax_2 = plot_SMPS(SMPS, SMPS_keys, SMPS['260422_vanillin+UV_dry_mass'].columns[42:-1], 'number and mass', 
                      timestamps, 10, ['Dry']*len(t_zero), 'Total concentration', t_zero, 2, 2, save_path)
 #%%
+# AMS
 for i, key in enumerate(AMS_keys):
     plot_AMS(AMS[key], None, t_zero[i], timestamps[i], HEPA_timestamps[i], 1, 'Dry', save_path)
 #%%
+# PTR-MS
 for i, key in enumerate(PTRMS_keys):
     fig, ax = plot_PTRMS_decay(PTRMS[key], PTRMS[key].keys()[5], [PTRMS[key].keys()[4]], 
                                ['C$_{8}$H$_{9}$O$_{3}^{+}$', 'C$_{7}$H$_{9}$O$_{2}^{+}$'], 
                                t_zero[i+2], t_UV_off[i], timestamps[i+2][1], 'Dry')
     fig.tight_layout()
     fig.savefig(f'{save_path}{t_zero[i+1].split(' ')[0]}_PTRMS_initial.jpg', dpi = 600)
-#%%
-for i, time in enumerate(timestamps):
-    plot_AURA_overview(DAQ[DAQ_keys[i]], SMPS[SMPS_keys[0][i]], AMS[AMS_keys[i]], time, t_zero[i], 'Dry', save_path)
