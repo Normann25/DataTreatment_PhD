02266516fc3342ed5dffa5f09c4b4393d372a831
Normann25 <151392271+Normann25@users.noreply.github.com>
2026-09-17 18:32:17 +0200

    Update 2608_Blanks_UV.py

--- Vanillin/2608_Blanks_UV.py
+++ Vanillin/2608_Blanks_UV.py
@@ -12,62 +12,50 @@ warnings.filterwarnings('ignore')
 pd.options.mode.chained_assignment = None  # suppress warnings
 #%%
 parent_path = '../../../Data/2026/'
-paths = ['20260824_blank_UV/', '20260827_Blank_UV_85RH/']
+paths = ['20260824_blank_UV/', '20260827_Blank_UV_85RH/', '20260828_Blank_UV/', '20260908_vanillin70ppb_UV_dry/', '20260909_vanillin70ppb_UV_85RH/']
 
 timestamps = [['2026-08-24 12:45', '2026-08-24 18:21'],
-              ['2026-08-27 08:20', '2026-08-27 13:45']]
-t_zero = ['2026-08-24 13:20', '2026-08-27 08:40']
-t_UV_off = ['2026-08-24 17:21', '2026-08-27 12:45']
+              ['2026-08-27 08:20', '2026-08-27 13:45'],
+              ['2026-08-28 08:40', '2026-08-28 13:55'],
+              ['2026-09-08 10:30', '2026-09-08 17:20'],
+              ['2026-09-09 09:50', '2026-09-09 16:30']]
+t_zero = ['2026-08-24 13:20', '2026-08-27 08:40', '2026-08-28 08:55', '2026-09-08 12:20', '2026-09-09 11:30']
+t_UV_off = ['2026-08-24 17:21', '2026-08-27 12:45', '2026-08-28 12:55', '2026-09-08 16:20', '2026-09-09 15:30']
 
-RH = ['Dry', '85% RH']
+RH = ['Dry', '85% RH', 'Dry', 'Dry', '85% RH']
 
-SMPS = {}
-PTRMS = {}
+SMPS = import_SMPS(paths, parent_path, 0)
+PTRMS = import_PTRMS(paths, parent_path)
 # AMS = {}
-DAQ = {}
-for path in paths:
-    temp_SMPS = import_SMPS(f'{parent_path}{path}SMPS/', '', 0)
-    for key in temp_SMPS.keys():
-        SMPS[key] = temp_SMPS[key]
-        # SMPS[key] = temp_SMPS[key]
-    # temp_AMS = import_data(f'{parent_path}{path}AMS/', '', 't_series', '%d-%m-%Y %H:%M:%S', 0)
-    # for key in temp_AMS.keys():
-    #     if 'PToF' not in key:
-    #         temp_AMS[key].columns = ['t_series', 'HROrg', 'HRNO3', 'HRSO4', 'HRNH4', 'HRChl', 'Ratio_H_C', 'Ratio_O_C', 
-    #                         'familyCHN', 'familyCHO1', 'familyCHOgt1', 'familyCHO1N', 'familyCH', 'f43', 'f44', 'Time']
-    #         temp_AMS[key].loc[temp_AMS[key]['Time'] < pd.to_datetime(t), ['Ratio_H_C', 'Ratio_O_C']] = np.nan
-    #         # temp_AMS[key].loc[temp_AMS[key]['Ratio_O_C'] < -0.1, ['Ratio_H_C', 'Ratio_O_C']] = 0
-    #         # temp_AMS[key].loc[temp_AMS[key]['Ratio_O_C'] > 4, ['Ratio_H_C', 'Ratio_O_C']] = 0
-    #     AMS[key] = temp_AMS[key]
-    temp_ptrms = import_PTRMS(f'{parent_path}{path}PTRMS/', '')
-    for key in temp_ptrms.keys():
-        PTRMS[key] = temp_ptrms[key]
-    temp_daq = import_data(f'{parent_path}{path}DAQ/', '', 'DAQ_Timestamp_UTC', '%d-%m-%Y %H:%M:%S', 0)
-    for key in temp_daq.keys():
-        temp = remove_spikes(temp_daq[key], ['Temp_C'], 5)
-        DAQ[key] = temp
+DAQ = import_DAQ(paths, parent_path, 0)
+for key in DAQ.keys():
+    DAQ[key] = remove_spikes(DAQ[key], ['Temp_C'], 5)
 
 save_path = '../../../Figures/Vanillin/2608_Blanks/'
 
 for key in SMPS.keys():
     SMPS[key].rename(columns = {SMPS[key].columns[38]:'Total concentration'}, inplace = True)
-    if '85RH' in key:
-        SMPS[key].loc[SMPS[key]['Total concentration'] <= 3, ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = np.nan
-
+    if '85RH' in key or 'vanillin70ppb' in key:
+        SMPS[key].loc[SMPS[key]['Total concentration'] <= 10, ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = np.nan
+    if '260908' in key:
+        SMPS[key].loc[SMPS[key]['Time'] < pd.to_datetime(t_zero[3]) + pd.Timedelta(minutes = 45), ['Median (nm)', 'Mean (nm)', 'Geo. Mean (nm)', 'Mode (nm)']] = np.nan
 
 RH_mask = DAQ['DataDAQ_260826']['RH_Percent'] > 10
 DAQ['DataDAQ_260826'] = DAQ['DataDAQ_260826'][RH_mask]
 
-SMPS_keys = [['20260824_blank_uv_dry_number', '20260827_blank_uv_85RH_number'],
-             ['20260824_blank_uv_dry_mass', '20260827_blank_uv_85RH_mass']]
-DAQ_keys = ['DataDAQ_260824', 'DataDAQ_260826']
-PTRMS_keys = ['260824_blank_dry', '260827_blank_85RH']
+SMPS_blank = [['20260824_blank_uv_dry_number', '20260827_blank_uv_85RH_number', '20260824_blank_uv_dry_number'],
+             ['20260824_blank_uv_dry_mass', '20260827_blank_uv_85RH_mass', '20260824_blank_uv_dry_mass']]
+SMPS_rep = [['260908_vanillin+UV_dry_number', '260909_vanillin+UV_85RH_number'],
+            ['260908_vanillin+UV_dry_mass', '260909_vanillin+UV_85RH_mass']]
+DAQ_keys = ['DataDAQ_260824', 'DataDAQ_260826', 'DataDAQ_260828', 'DataDAQ_260908', 'DataDAQ_260909']
+PTRMS_keys = ['260824_blank_dry', '260827_blank_85RH', '260828_blank_dry', '260908_VL+UV_dry_fragments', '260909_VL+UV_85RH_fragments']
 #%%
+SMPS_number = SMPS_blank[0] + SMPS_rep[0]
 for i, time in enumerate(timestamps):
-    plot_AURA_overview(DAQ[DAQ_keys[i]], SMPS[SMPS_keys[0][i]], None, time, None, t_zero[i], RH[i], save_path)
+    plot_AURA_overview(DAQ[DAQ_keys[i]], SMPS[SMPS_number[i]], None, time, None, t_zero[i], RH[i], save_path)
 #%%
-ax, ax_2 = plot_SMPS(SMPS, SMPS_keys, SMPS['20260824_blank_uv_dry_mass'].columns[42:-1], 'number and mass', 
-                     timestamps, 10, RH, 'Total concentration', t_zero, 1, 2, save_path)
+ax, ax_2 = plot_SMPS(SMPS, SMPS_blank, SMPS['20260824_blank_uv_dry_mass'].columns[42:-1], 'number and mass', 
+                     timestamps[:3], 10, RH[:3], 'Total concentration', t_zero[:3], 1, 2, save_path)
 #%%
 # PTR-MS grouping of ions
 for key in PTRMS_keys:
